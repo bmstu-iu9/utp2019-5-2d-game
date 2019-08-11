a61b32dc3ad9d5e6f8e64f978b3ccb9a047f48d3
Andrey Chernenkov <andrey.chernenkov@mail.ru>
2019-08-11 20:59:16 +0300

    Created routing for view

--- index.js
+++ index.js
@@ -3,6 +3,20 @@ var express = require('express');
 
 var app = express();
 
-app.get('/cabinet', function(req, res) {
-    res.
+
+app.use('/view', express.static('view'));
+
+app.use('/:name', function(req, res) {
+    let name = req.params.name;
+    switch(name) {
+        case 'cabinet': case 'input': case 'registration': 
+            return res.sendFile(__dirname + '/view/html/' + name + '.html');
+        default: 
+            return res.sendStatus(404);
+    }
+});
+
+app.listen(3012, function() {
+    console.log('API started');
 })
+
