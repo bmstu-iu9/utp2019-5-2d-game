b9152f193d29ff06f39e200f3d47de487db70a4a
Andrey Chernenkov <andrey.chernenkov@mail.ru>
2019-08-11 22:17:26 +0300

    pre-commit

--- index.js
+++ index.js
@@ -7,12 +7,36 @@ var logger = require("./logger");
 
 var app = express();
 
+/*
+var express = require('express');
+
+var app = express();
+
+
+
+
+app.use('/:name', function(req, res) {
+    let name = req.params.name;
+    switch(name) {
+        case 'cabinet': case 'input': case 'registration': 
+            return res.sendFile(__dirname + '/view/html/' + name + '.html');
+        default: 
+            return res.sendStatus(404);
+    }
+});
+
+app.listen(3012, function() {
+    console.log('API started');
+})
+
+*/
 var bodyParser = require("body-parser")
 
 var levelRouter = require("./routes/level")
 
 app.use(bodyParser.urlencoded({ extended: false}))
 app.use(bodyParser.json());
+app.use(express.static('view'));
 
 app.use('/levels', levelRouter);
 
