7a3a26a73ea0b4cfed8c53df3418c7bb31e3e298
Veronika Aleksandrova <veronika.aleksandrova.00@bk.ru>
2019-08-14 13:02:15 +0500

    Cleaned up the html Renamed the view folder to public

--- index.js
+++ index.js
@@ -4,13 +4,13 @@ var express = require('express');
 var app = express();
 
 
-app.use(express.static('view'));
+app.use(express.static('public'));
 
 app.use('/:name', function(req, res) {
     let name = req.params.name;
     switch(name) {
         case 'cabinet': case 'input': case 'registration': case 'search':
-            return res.sendFile(__dirname + '/view/html/' + name + '.html');
+            return res.sendFile(__dirname + '/public/html/' + name + '.html');
         default:
             return res.sendStatus(404);
     }
