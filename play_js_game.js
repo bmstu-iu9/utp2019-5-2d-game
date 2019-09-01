b64b0407e011b564aa6e79f9cf8c16b87458ce4b
1grim1 <52464868+1grim1@users.noreply.github.com>
2019-09-01 23:13:09 +0300

    Add files via upload

--- play_js_game.js
+++ play_js_game.js
@@ -20,7 +20,7 @@ var PI = Math.PI;
 //прописать получение обьектов
 //!!
 
-var rect = [],player,finish;//присвоить  обьекты
+var rect = [],player,finish,i;//присвоить  обьекты
 
 var move = {
   w:true,
@@ -57,3 +57,83 @@ document.addEventListener("keydown",function(e){
       move.a = true;
   }
 });
+
+for(i in rect){
+
+    if(rect[i].type == 1){
+
+        ctx.beginPath();
+
+        ctx.fillStyle = rect[i].color;
+        ctx.strokeStyle = rect[i].color;
+        ctx.arc(rect[i].x0,rect[i].y0,rect[i].width,0,Math.PI * 2,true);
+
+        ctx.fill();
+
+      };
+
+    if(rect[i].type == 2){
+
+      ctx.fillStyle = rect[i].color;
+
+      ctx.strokeStyle = rect[i].color;
+
+      ctx.lineWidth = rect[i].width;
+
+      ctx.beginPath();
+
+      ctx.moveTo(rect[i].x1,rect[i].y1);
+
+      ctx.lineTo(rect[i].x0,rect[i].y0);
+
+      ctx.stroke();
+
+      ctx.closePath();
+
+    };//отрисовка линии соеденяющиеся с кружочками(что бы было ровно)
+
+    if(rect[i].type == 3){
+
+      ctx.strokeStyle = rect[i].color;
+
+      ctx.fillStyle = rect[i].color;
+
+      ctx.lineWidth = rect[i].width;
+
+      ctx.closePath();
+
+      ctx.beginPath();
+
+      ctx.moveTo(rect[i].x1,rect[i].y1);
+
+      ctx.lineTo(rect[i].x0,rect[i].y0);
+
+      ctx.stroke();
+
+      ctx.closePath();
+    };//отрисовка прямых линий
+
+    if(rect[i].type == 4){
+
+      ctx.fillStyle = rect[i].color;
+
+      ctx.beginPath();
+
+      ctx.fillRect(rect[i].x0,rect[i].y0,rect[i].x1,rect[i].y1);
+
+      ctx.closePath();
+
+    };//отрисовка прямоугольников
+
+    if(rect[i].type == 5){
+      ctx.fillStyle = rect[i].color;
+      ctx.beginPath();
+
+      ctx.arc(rect[i].x0, rect[i].y0, rect[i].R,0 , 2 * PI,false);
+
+      ctx.fill();
+
+      ctx.closePath();
+      //draw_buttons();
+    };
+  };
